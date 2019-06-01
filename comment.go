387a1b4986f413e8d2504a0f5b593d6ffbe00caa
qianlAIyf <37335349+qianlAIyf@users.noreply.github.com>
2019-06-01 16:41:30 +0800

    Update comment.go

--- comment.go
+++ comment.go
@@ -6,15 +6,15 @@ import (
 )
 
 type CommentForm struct {
-	Uid string `json:"uid" form:"uid"`
-	Mid	string `json:"mid" form:"mid"`
+	Uid     string `json:"uid" form:"uid"`
+	Mid     string `json:"mid" form:"mid"`
 	Content string `json:"content" form:"content"`
 }
 
 func HandleGetAllComment(c *gin.Context) {
 	var req CommentForm
 	if err := c.Bind(&req); err != nil {
-		writeResponse(c,-1,err.Error(),nil)
+		writeResponse(c, -1, err.Error(), nil)
 		return
 	}
 	res, err := dal.GetAllComment(req.Mid)
@@ -27,22 +27,22 @@ func HandleGetAllComment(c *gin.Context) {
 	//	res[key]["name"] = name
 	//}
 	if err != nil {
-		writeResponse(c,-1,err.Error(),nil)
+		writeResponse(c, -1, err.Error(), nil)
 		return
 	}
-	writeResponse(c,0,"",res)
+	writeResponse(c, 0, "", res)
 }
 
-func HandleCreateComment(c *gin.Context){
+func HandleCreateComment(c *gin.Context) {
 	var req CommentForm
 	if err := c.Bind(&req); err != nil {
-		writeResponse(c,-1,err.Error(),nil)
+		writeResponse(c, -1, err.Error(), nil)
 		return
 	}
-	err := dal.CreateComment(req.Uid,req.Mid,req.Content)
+	err := dal.CreateComment(req.Uid, req.Mid, req.Content, dsn)
 	if err != nil {
-		writeResponse(c,-1,err.Error(),nil)
+		writeResponse(c, -1, err.Error(), nil)
 		return
 	}
-	writeResponse(c,0,"success", err)
+	writeResponse(c, 0, "success", err)
 }
